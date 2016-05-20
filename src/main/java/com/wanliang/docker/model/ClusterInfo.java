dd0a5a8ca98a4c1f6eaa5776aacd21aa57b22060
wan_liang@126.com <wan_liang@126.com>
2016-05-20 13:39:36 +0800

    commit

---
+++ src/main/java/com/wanliang/docker/model/ClusterInfo.java
@@ -0,0 +1,46 @@
+package com.wanliang.docker.model;
+
+/**
+ * Created by Administrator on 2016/5/18.
+ */
+public class ClusterInfo {
+
+    private int containersRunning;
+    private int containersStopped;
+    private int images;
+
+    private String operatingSystem;
+
+    public int getContainersRunning() {
+        return containersRunning;
+    }
+
+    public void setContainersRunning(int containersRunning) {
+        this.containersRunning = containersRunning;
+    }
+
+    public int getContainersStopped() {
+        return containersStopped;
+    }
+
+    public void setContainersStopped(int containersStopped) {
+        this.containersStopped = containersStopped;
+    }
+
+    public int getImages() {
+        return images;
+    }
+
+    public void setImages(int images) {
+        this.images = images;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+}
